7e222a1625e79805f6087b096a5e6ae7c97c1bd3
Chelsea Kerr <54897046+ckerr8@users.noreply.github.com>
2024-09-10 13:30:36 -0400

    add cors middleware

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import express from "express";
 import warehouseRoutes from "./routes/warehouseRoute.js";
 import inventoryRoutes from "./routes/inventoryRoute.js";
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
+app.use(cors());
 
 app.use(express.json());
 
